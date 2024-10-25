76fb692826883f4e339a6c38b8b22d33872fdcc2
NeuroTechUCB <92194873+ntxucb@users.noreply.github.com>
2024-10-24 22:33:06 -0400

    Update TaskBar.jsx

--- src/routes/Components/TaskBar.jsx
+++ src/routes/Components/TaskBar.jsx
@@ -7,7 +7,7 @@ export function TaskBar() {
   return (
     <Navbar style={{ backgroundColor: "black" }}>
       <Container>
-        <Navbar.Brand as={Link} to="/home">
+        <Navbar.Brand as={Link} to="/ntxucblapaz.github.io/home">
           <img
             src={NTX_Logo}
             width="120"
@@ -18,19 +18,19 @@ export function TaskBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/home" className="nav-link">
+            <Nav.Link as={Link} to="/ntxucblapaz.github.io/home" className="nav-link">
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/projects" className="nav-link">
+            <Nav.Link as={Link} to="/ntxucblapaz.github.io/projects" className="nav-link">
               Projects
             </Nav.Link>
-            <Nav.Link as={Link} to="/events" className="nav-link">
+            <Nav.Link as={Link} to="/ntxucblapaz.github.io/events" className="nav-link">
               Events
             </Nav.Link>
-            <Nav.Link as={Link} to="/podcast" className="nav-link">
+            <Nav.Link as={Link} to="/ntxucblapaz.github.io/podcast" className="nav-link">
               Podcast
             </Nav.Link>
-            <Nav.Link as={Link} to="/our_team" className="nav-link">
+            <Nav.Link as={Link} to="/ntxucblapaz.github.io/our_team" className="nav-link">
               Our Team
             </Nav.Link>
           </Nav>
