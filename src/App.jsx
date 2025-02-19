a10ce862dbb83f8d7604462d607a6f775b7cc20c
IsaiDiaz <jsoza057@gmail.com>
2025-02-18 20:26:41 -0400

    feat: Agregar pagina 'Under construction'

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { Events } from "./pages/Events/Events";
 import { Our_Team } from "./pages/Our_Team/Our_Team";
 import { Podcast } from "./pages/Podcast/Podcast";
 import { Projects } from "./pages/Projects/Projects";
+import config from "./config.json"
+import UnderConstruction from "./pages/UnderConstruction/UnderConstruction";
 
-function App() {
+function AppRoutes() {
+  const location = useLocation();
+  const isUnderConstruction = config.pagesUnderConstruction.includes(location.pathname);
+
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
+    <Routes>
+      {isUnderConstruction ? (
+        <Route path={location.pathname} element={<UnderConstruction />} />
+      ) : (
+        <>
           <Route path="/ntxucblapaz.github.io/" element={<Home />} />
           <Route path="/ntxucblapaz.github.io/events" element={<Events />} />
           <Route path="/ntxucblapaz.github.io/our_team" element={<Our_Team />} />
           <Route path="/ntxucblapaz.github.io/podcast" element={<Podcast />} />
           <Route path="/ntxucblapaz.github.io/projects" element={<Projects />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+        </>
+      )}
+    </Routes>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/UnderConstruction/UnderConstruction.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { TaskBar } from "../../components/layout/TaskBar/TaskBar";
+import Footer from "../../components/layout/Footer/Footer";
+import { FaTools } from 'react-icons/fa'; 
+import "./UnderConstruction.css";
+
+const UnderConstruction = () => {
+  return (
+    <>
+      <TaskBar />
+      <div className="forbidden-container">
+        <div className="forbidden-content">
+          <div className="forbidden-icon">
+            <FaTools size={80} color="#FE8A9F" /> 
+          </div>
+          <h1 className="forbidden-title">🚧</h1>
+          <h2 className="forbidden-subtitle">Page Under Construction</h2>
+          <p className="forbidden-message">
+            Sorry, we are working on something amazing. Come back soon!
+          </p>
+          <a href="/" className="back-button">
+            <i className="fa fa-arrow-left"></i> 
+            Go Back
+          </a>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default UnderConstruction;
