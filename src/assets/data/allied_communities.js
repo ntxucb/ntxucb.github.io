39412659ce58d1293b29e86218ed93ce6f97f88a
IsaiDiaz <jsoza057@gmail.com>
2025-04-20 21:58:46 -0400

    feature: comudidades aliadas agregadas a la pagina events

---
+++ src/assets/data/allied_communities.js
@@ -0,0 +1,129 @@
+import caosbo from '../images/allied communities/CAOSBOL - Arduino Open Source Community BO_LPZ.png'
+import notionBolivia from '../images/allied communities/Notion-Bolivia.jpg'
+import sociedadCientíficaAlfredNobel from '../images/allied communities/Logo_SCE_INB - Jose Luis Alcazar Von Landwust.png'
+import mujeresTicsBolivia from '../images/allied communities/LOGO MUJERES TICs-04 - Karen Lara.png'
+import ethereumBolivia from '../images/allied communities/15_isotipo_magenta - Huáscar Miranda Martinez.png'
+import sociedadCientíficaMecatrónica from '../images/allied communities/LOGO_SCE_1 - LITZY XIMENA CONDE ALVARADO.png'
+import nosGustaLaCiencia from '../images/allied communities/IMG_0779 - Emily claris Zabaleta paco.jpeg'
+import cienciaLinkBolivia from '../images/allied communities/logo alt copia letras - Ricardo Orozco.png'
+import flutterDevsBolivia from '../images/allied communities/logo fdb - Marco Leonardini.png'
+import neurotransmitiendo from '../images/allied communities/NT_logo_verde_negativo_transparente - Mili Vera.png'
+import microsoftUserGroupBolivia from '../images/allied communities/MUGBo Horizontal Oficial - Diana Calizaya.png'
+import mujercitas from '../images/allied communities/a color - Edwin Poma.png'
+import womenInFirebase from '../images/allied communities/Women in Firebase logo - ADRIANA TICONIPA GUTIERREZ.png'
+import pyladiesLaPaz from '../images/allied communities/pyladieslp_outlinerosa_transparente - La Paz PyLadies.png'
+import hacklabBrickheads from '../images/allied communities/large_dark - Hacklab Brickheads.png'
+import buidlersBolivia from '../images/allied communities/Banner buidlers Bolivia - HANSA Proyectos y servicios.png'
+import angularBolivia from '../images/allied communities/Group 177 - Rodrigo Torrico.png'
+import bitcoinResearch from '../images/allied communities/Bitcoin Reseach Logo - Carles Romy Andres Quispe Tarqui.jpg'
+
+export const alliedCommunitiesData = [
+    {
+        "id": 1,
+        "name": "CAOSBO",
+        "logo": caosbo,
+        "social_media": "https://www.instagram.com/caosbol"
+    },
+    {
+        "id": 2,
+        "name": "Notion Bolivia",
+        "logo": notionBolivia,
+        "social_media": "https://www.instagram.com/notionbolivia/"
+    },
+    {
+        "id": 3,
+        "name": "Sociedad Científica Estudiantil \"Alfred Nobel\" de Ingeniería Biomédica",
+        "logo": sociedadCientíficaAlfredNobel,
+        "social_media": "https://www.instagram.com/sce.inb/"
+    },
+    {
+        "id": 4,
+        "name": "Mujeres TICs Bolivia",
+        "logo": mujeresTicsBolivia,
+        "social_media": "https://www.instagram.com/mujeresticsbo"
+    },
+    {
+        "id": 5,
+        "name": "Ethereum Bolivia ",
+        "logo": ethereumBolivia,
+        "social_media": "https://www.instagram.com/ethereumbo"
+    },
+    {
+        "id": 6,
+        "name": "Sociedad Científica Estudiantil de Ingeniería Mecatrónica",
+        "logo": sociedadCientíficaMecatrónica,
+        "social_media": "https://www.instagram.com/sce_imt_ucb/"
+    },
+    {
+        "id": 7,
+        "name": "Nos Gusta la Ciencia ",
+        "logo": nosGustaLaCiencia,
+        "social_media": "https://www.instagram.com/nosgustalaciencia?igsh=MXhwYmQydzBuOWUyNw=="
+    },
+    {
+        "id": 8,
+        "name": "Ciencia Link Bolivia",
+        "logo": cienciaLinkBolivia,
+        "social_media": "https://www.instagram.com/ciencialinkbolivia/"
+    },
+    {
+        "id": 9,
+        "name": "Flutter Devs Bolivia",
+        "logo": flutterDevsBolivia,
+        "social_media": "none"
+    },
+    {
+        "id": 10,
+        "name": "NeuroTransmitiendo",
+        "logo": neurotransmitiendo,
+        "social_media": "https://www.instagram.com/neurotransmitiendo/"
+    },
+    {
+        "id": 11,
+        "name": "Microsoft User Group Bolivia",
+        "logo": microsoftUserGroupBolivia,
+        "social_media": "https://www.instagram.com/microsoftusergroup.bo/"
+    },
+    {
+        "id": 12,
+        "name": "Mujercitas",
+        "logo": mujercitas,
+        "social_media": "https://www.instagram.com/mujercitasbol?igsh=Y2Rva2Q5cnRxbHlk"
+    },
+    {
+        "id": 13,
+        "name": "Women+ in Firebase",
+        "logo": womenInFirebase,
+        "social_media": "https://www.instagram.com/womeninfirebase/ "
+    },
+    {
+        "id": 14,
+        "name": "PyLadies La Paz",
+        "logo": pyladiesLaPaz,
+        "social_media": "https://www.instagram.com/pyladieslapaz/"
+    },
+    {
+        "id": 15,
+        "name": "HackLab BrickHeads",
+        "logo": hacklabBrickheads,
+        "social_media": "https://www.instagram.com/hacklabbrickheads/"
+    },
+    {
+        "id": 16,
+        "name": "Buidlers Bolivia",
+        "logo": buidlersBolivia,
+        "social_media": "https://www.instagram.com/buidlersbo/"
+    },
+    {
+        "id": 17,
+        "name": "Angular Bolivia",
+        "logo": angularBolivia,
+        "social_media": "@angularbolivia"
+    },
+    {
+        "id": 18,
+        "name": "Bitcoin Research",
+        "logo": bitcoinResearch,
+        "social_media": "https://www.instagram.com/bitcoinr3/"
+    },
+];
